aaad0314c627b62feac61c4c067622e8d95a562b
Colton Dowling <Colton@cd3style.com>
2017-08-14 14:15:39 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,11 @@
+const moment = require('moment');
+const chalk = require ('chalk');
+
+
+const opp= "It is"
+const line1 = opp+  Tuesday, July 18th 2017, 3:07:48
+const line2 = opp+ "the" 199th "day of the year"
+const line3 = opp + 599 seconds into the day
+const line4 = "It" + is+ "day lights savings time"
+const line5 = opp+ not "a leap year"
+});
